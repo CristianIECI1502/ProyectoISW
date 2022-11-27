e2dae4da37232e56dd8afb1655f2e4dc9e590849
Claudio Rodriguez <cluadio.rodriguez1801@alumnos.ubiobio.cl>
2022-11-27 17:46:55 -0300

    update fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,60 +15,60 @@ const createUser = (req, res)=>{
         return res.status(200).send(user)
     })
 }
-const getUser =(req, res)=>{
-    User.find({},(err, user) =>{
+const getUsers =(req, res)=>{
+    User.find({},(err, users) =>{
         if(err){
             return res.status(400).send({message:'Error al obtener a los usuario'})
         }
         if (!user) {
             return res.status(404).send({ message: "No hay usuarios disponibles."})
         }
-        return res.status(200).send(user)
+        return res.status(200).send(users)
     })
 }
 
 const GetSpecificUser = (req, res) => {
     const { id } = req.params;
-    User.findById(id, (err, user) => {
+    User.findById(id, (err, users) => {
         if (err) {
             return res.status(400).send({ message: "Error al obtener el usuario"})
         }
-        if (!user) {
+        if (!users) {
             return res.status(404).send({ message: "Usuario no encontrado"})
         }
-        return res.status(200).send(User)
+        return res.status(200).send(users)
     })
 }
 
 const updateUser=(req, res) =>{
     const { id } = req.params;
-    User.findByIdAndUpdate(id, req.body,(err,user)=>{
+    User.findByIdAndUpdate(id, req.body,(err,users)=>{
         if (err){
             return res.status(400).send({message:"Error al encontrar los Usuario"})
         }
-        if(!user){
+        if(!users){
             return res.status(404).send({message: "Usuario no existe"})
         }
-        return res.status(200).send(user)
+        return res.status(200).send(users)
     })
 }
 
 const deleteUser=(req, res)=>{
     const { id }=req.params;
-    User.findByIdAndDelete(id, req.body,(err,user)=>{
+    User.findByIdAndDelete(id, req.body,(err,users)=>{
         if(err){
             return res.status(400).send({message:"Error al encontrar el Usuario"})
         }
-        if(!user){
+        if(!users){
             return res.status(404).send({message:"Usuario no Existe"})
         }
-        return res.status(200).send(user)
+        return res.status(200).send(users)
     })
 }
 
 module.exports ={
     createUser,
-    getUser,
+    getUsers,
     GetSpecificUser,
     updateUser,
     deleteUser
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,8 @@ const api = express.Router();
 const userController =require('../controllers/userController');
 
 api.post('/createuser',userController.createUser);
-api.get('/user/search',userController.getUser);
-api.get('/user/search/specific',userController.GetSpecificUser);
+api.get('/users/search',userController.getUsers);
+api.get('/users/search/specific',userController.GetSpecificUser);
 api.put('/user/update/:id',userController.updateUser);
 api.delete('/user/delete/:id',userController.deleteUser);
 
