f474373aa31b0790134edd8578fa5d585fef77ca
Claudio Rodriguez <cluadio.rodriguez1801@alumnos.ubiobio.cl>
2022-11-28 01:50:04 -0300

    update users

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const user = require('../models/user');
 const User = require('../models/user');
 
 const createUser = (req, res)=>{
-    const{name,password,rut}=req.body;
+    const{identificador_casa,name,password,rut,admin}=req.body;
     const newUser = new User({
         identificador_casa,
         name,
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController =require('../controllers/userController');
 
 api.post('/createuser',userController.createUser);
 api.get('/users/search',userController.getUsers);
-api.get('/users/search/specific',userController.GetSpecificUser);
+api.get('/users/search/specific/:id',userController.GetSpecificUser);
 api.put('/user/update/:id',userController.updateUser);
 api.delete('/user/delete/:id',userController.deleteUser);
 
